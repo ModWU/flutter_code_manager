248558fbe5fb8f3b841b7e4b0a1ecf143d61a73c
wuchaochao <wuchaochao@shandagames.com>
2021-04-29 20:35:32 +0800

    update data with bugs => not solved

--- projects/list_viewport_manager/lib/list_demo.dart
+++ projects/list_viewport_manager/lib/list_demo.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -22,33 +23,105 @@ class MyAppState extends State<MyApp> {
   /*List<double> _heights;
   List<Key> _keys;*/
 
-  late List<double> _initHeights;
+  List<double>? _initHeights;
 
   late ListModel _listModel;
 
   late StateController _stateController;
   int? _nearCenterIndex;
 
+  late ScrollController _scrollController;
+  double _listHeight = 450;
+
+  bool _childFixHeight = false;
+
+  void updateHeights() {
+    final Random rd = Random();
+    final int count = _initHeights?.length ?? rd.nextInt(20) + 15;
+    _initHeights = List.generate(count, (index) => rd.nextDouble() * 100 + 40);
+  }
+
+  void resetListHeight() {
+    final Random rd = Random();
+    _listHeight = rd.nextDouble() * 350 + 100;
+  }
+
   @override
   void initState() {
     super.initState();
-    final Random rd = Random();
-    final int count = rd.nextInt(20) + 15;
-    _initHeights = List.generate(count, (index) => rd.nextDouble() * 100 + 40);
+    updateHeights();
     _listModel = ListModel(
-      dataList: _initHeights.map((e) => "$e").toList(),
+      dataList: _initHeights!.map((e) => "$e").toList(),
     );
     _stateController = Get.put(StateController.fill(_listModel.length));
+    _scrollController = ScrollController();
   }
 
   @override
   void dispose() {
     _listModel.dispose();
+    _scrollController.dispose();
     super.dispose();
   }
 
-  void _initHeightsByScroll(ScrollMetrics metrics) {
-    print("");
+  Widget _buildListView() {
+    return Container(
+      height: _listHeight,
+      width: double.infinity,
+      color: Colors.black12,
+      child: _listModel.buildList(
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            physics: BouncingScrollPhysics(),
+            controller: _scrollController,
+            reverse: false,
+            itemBuilder: (context, index) {
+              return _listModel.buildChild(
+                index: index,
+                child: Obx(() {
+                  final nearCenter = _stateController.testStates[index].value;
+                  return Container(
+                    height: !_childFixHeight || !nearCenter
+                        ? _initHeights![index]
+                        : 20,
+                    width: _initHeights![index],
+                    color: nearCenter
+                        ? Colors.white
+                        : Colors.primaries[index % Colors.primaries.length],
+                    alignment: Alignment.center,
+                    child: Text(
+                      nearCenter
+                          ? "$index => center"
+                          : "$index => ${_listModel[index]}",
+                      style: TextStyle(
+                        fontSize: nearCenter ? 16 : 12,
+                        color: nearCenter ? Colors.black : Colors.white,
+                        fontStyle:
+                            nearCenter ? FontStyle.italic : FontStyle.normal,
+                      ),
+                    ),
+                  );
+                }),
+              );
+            },
+            itemCount: _initHeights!.length,
+          ),
+          onUpdate: (IScrollDataInterface scrollDataInterface) {
+            print(
+                "onUpdate =>maxScrollExtent: ${scrollDataInterface.scrollMetrics.maxScrollExtent}}, firstVisibleIndex: ${scrollDataInterface.firstVisibleIndex}, lastVisibleIndex: ${scrollDataInterface.lastVisibleIndex}");
+            _updateNearCenterItem(scrollDataInterface);
+          },
+          onScrollStart: (IScrollDataInterface scrollDataInterface) {},
+          onScrollEnd: (IScrollDataInterface scrollDataInterface) {
+            print(
+                "onScrollEnd => maxScrollExtent: ${scrollDataInterface.scrollMetrics.maxScrollExtent}}");
+          },
+          onScrollUpdate: (IScrollDataInterface scrollDataInterface) {
+            print(
+                "onScrollUpdate => maxScrollExtent: ${scrollDataInterface.scrollMetrics.maxScrollExtent}, firstVisibleIndex: ${scrollDataInterface.firstVisibleIndex}, lastVisibleIndex: ${scrollDataInterface.lastVisibleIndex}, viewportDimension: ${scrollDataInterface.scrollMetrics.viewportDimension}, pixels: ${scrollDataInterface.scrollMetrics.pixels}");
+            _updateNearCenterItem(scrollDataInterface);
+          }),
+    );
   }
 
   @override
@@ -57,67 +130,91 @@ class MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(),
         body: Center(
-          child: Container(
-            height: 500,
-            width: 400,
-            color: Colors.black12,
-            child: _listModel.buildList(
-                child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  physics: BouncingScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    return _listModel.buildChild(
-                      index: index,
-                      child: Obx(() {
-                        final nearCenter =
-                            _stateController.testStates[index].value;
-                        print("index: $index => nearCenter: $nearCenter");
-                        return Container(
-                          height: _initHeights[index],
-                          width: _initHeights[index],
-                          color: nearCenter
-                              ? Colors.white
-                              : Colors
-                                  .primaries[index % Colors.primaries.length],
-                          alignment: Alignment.center,
-                          child: Text(
-                            nearCenter ? "$index => center" : "$index => ${_listModel[index]}",
-                            style: TextStyle(
-                              fontSize: nearCenter ? 16 : 12,
-                              color: nearCenter ? Colors.black : Colors.white,
-                              fontStyle: nearCenter ? FontStyle.italic : FontStyle.normal,
-                            ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Stack(
+                  children: [
+                    _buildListView(),
+                    Positioned(
+                      child: Center(
+                        child: Text(
+                          "center",
+                          style: TextStyle(
+                            color: Colors.redAccent,
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 10.5,
+                            backgroundColor: Colors.white,
                           ),
-                        );
-                      }),
-                    );
-                  },
-                  itemCount: _initHeights.length,
+                        ),
+                      ),
+                      top: 0,
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                    ),
+                    Positioned(
+                      child: Divider(
+                        height: 2.0,
+                        color: Colors.black,
+                      ),
+                      top: 0,
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                    ),
+                  ],
                 ),
-                onScrollStart: (IScrollDataInterface scrollDataInterface,
-                    ScrollMetrics metrics) {},
-                onScrollEnd: (IScrollDataInterface scrollDataInterface,
-                    ScrollMetrics metrics) {
-
-                },
-                onScrollUpdate: (IScrollDataInterface scrollDataInterface,
-                    ScrollMetrics metrics) {
-                  if (scrollDataInterface.nearCenterVisibleIndex !=
-                      _nearCenterIndex) {
-                    _stateController
-                        .testStates[scrollDataInterface.nearCenterVisibleIndex]
-                        .value = true;
-                    if (_nearCenterIndex != null) {
-                      _stateController.testStates[_nearCenterIndex!].value =
-                      false;
-                    }
-                    _nearCenterIndex =
-                        scrollDataInterface.nearCenterVisibleIndex;
-                  }
-                }),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          resetListHeight();
+                        });
+                      },
+                      child: Text("改变高度"),
+                    ),
+                    ChoiceChip(
+                      label: Text('孩子定高'),
+                      selected: _childFixHeight,
+                      onSelected: (v) {
+                        setState(() {
+                          _childFixHeight = v;
+                        });
+                      },
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          updateHeights();
+                        });
+                      },
+                      child: Text("改变数据"),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _updateNearCenterItem(IScrollDataInterface scrollDataInterface) {
+    if (!scrollDataInterface.hasVisibleChild) return;
+
+    if (scrollDataInterface.nearCenterVisibleIndex != _nearCenterIndex) {
+      _stateController
+          .testStates[scrollDataInterface.nearCenterVisibleIndex].value = true;
+      if (_nearCenterIndex != null) {
+        _stateController.testStates[_nearCenterIndex!].value = false;
+      }
+      _nearCenterIndex = scrollDataInterface.nearCenterVisibleIndex;
+    }
   }
 }
--- projects/list_viewport_manager/lib/list_viewport_manager.dart
+++ projects/list_viewport_manager/lib/list_viewport_manager.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'layout_callback_builder.dart';
+import 'dart:math';
 
-typedef OnScrollStart = void Function(
-    IScrollDataInterface scrollDataInterface, ScrollMetrics metrics);
+typedef OnScrollStart = void Function(IScrollDataInterface scrollDataInterface);
 typedef OnScrollUpdate = void Function(
-    IScrollDataInterface scrollDataInterface, ScrollMetrics metrics);
-typedef OnScrollEnd = void Function(
-    IScrollDataInterface scrollDataInterface, ScrollMetrics metrics);
+    IScrollDataInterface scrollDataInterface);
+typedef OnScrollEnd = void Function(IScrollDataInterface scrollDataInterface);
+typedef OnUpdate = void Function(IScrollDataInterface scrollDataInterface);
 
 abstract class IScrollDataInterface {
   factory IScrollDataInterface(_ListLayoutManager listLayoutManager) =>
@@ -15,36 +15,49 @@ abstract class IScrollDataInterface {
   int get firstVisibleIndex;
   int get lastVisibleIndex;
   int get nearCenterVisibleIndex;
-  GestureDirection get gestureDirection;
+  bool get hasVisibleChild;
+  ScrollMetrics get scrollMetrics;
+  GestureDirection? get gestureDirection;
   ComputedSize getComputedSize(int index);
   List<ComputedSize> get visibleComputedSizes;
 }
 
 class _ImplScrollDataInterface implements IScrollDataInterface {
-  final _ListLayoutManager _listLayoutManager;
+  _ListLayoutManager? _listLayoutManager;
 
   _ImplScrollDataInterface._(this._listLayoutManager);
 
   @override
-  int get firstVisibleIndex => _listLayoutManager._firstVisibleIndex!;
+  int get firstVisibleIndex => _listLayoutManager!._firstVisibleIndex!;
 
   @override
-  int get lastVisibleIndex => _listLayoutManager._lastVisibleIndex!;
+  int get lastVisibleIndex => _listLayoutManager!._lastVisibleIndex!;
 
   @override
-  int get nearCenterVisibleIndex => _listLayoutManager._nearCenterVisibleIndex!;
+  int get nearCenterVisibleIndex =>
+      _listLayoutManager!._nearCenterVisibleIndex!;
 
   @override
-  GestureDirection get gestureDirection =>
-      _listLayoutManager._gestureDirection!;
+  ScrollMetrics get scrollMetrics => _listLayoutManager!._scrollMetrics!;
+
+  @override
+  GestureDirection? get gestureDirection =>
+      _listLayoutManager!._gestureDirection;
 
   @override
   ComputedSize getComputedSize(int index) =>
-      _listLayoutManager._getComputedSize(index);
+      _listLayoutManager!._getComputedSize(index);
 
   @override
   List<ComputedSize> get visibleComputedSizes =>
-      _listLayoutManager._visibleComputedSizes;
+      _listLayoutManager!._visibleComputedSizes;
+
+  void _dispose() {
+    _listLayoutManager = null;
+  }
+
+  @override
+  bool get hasVisibleChild => firstVisibleIndex >= 0;
 }
 
 enum GestureDirection {
@@ -89,17 +102,129 @@ mixin _ListLayoutManager on _ListSizeManager {
   int? _firstVisibleIndex, _lastVisibleIndex; //可能为-1，都看不见
   int? _nearCenterVisibleIndex;
   double? _currentPosition;
+  ScrollMetrics? _scrollMetrics;
+  ScrollController? _scrollController;
 
-  late IScrollDataInterface _scrollDataInterface = IScrollDataInterface(this);
+  bool _updateMetricsFlag = false;
+  bool _updateSizeFlag = false;
+  OnUpdate? _onUpdate;
+
+  int? _minIndexLayout, _maxIndexLayout;
+  Map<int, double>? _cacheSizes;
+
+  late IScrollDataInterface? _scrollDataInterface = IScrollDataInterface(this);
+
+  @override
+  void dispose() {
+    _clearData();
+    (_scrollDataInterface! as _ImplScrollDataInterface)._dispose();
+    _scrollDataInterface = null;
+    super.dispose();
+  }
 
   Widget buildChild({required int index, required Widget child}) {
     return LayoutCallbackBuilder(
         layoutCallback: (Size childSize) {
           final size =
               _axis == Axis.horizontal ? childSize.width : childSize.height;
-          _updateHeightByLayout(index, size);
+          final ComputedSize? oldComputedSize =
+              _getSizeLength() <= index ? null : _getComputedSize(index);
+          print(
+              "layout => index: $index => newSize: $size, oldSize: ${oldComputedSize?.size ?? 0}");
+          //_updateHeightByLayout(index, size);
+          _cacheSizes ??= {};
+          _cacheSizes![index] = size;
+
+          _maxIndexLayout ??= index;
+          _maxIndexLayout = max(_maxIndexLayout!, index);
+          print(
+              "update assert before");
+          if (_isNeedUpdateIndex(index, size)) {
+            print(
+                "update assert after");
+            _minIndexLayout ??= index;
+            _minIndexLayout = min(_minIndexLayout!, index);
+            _markUpdateSizesAfterLayout();
+            print(
+                "update assert after----------");
+          }
+          print(
+              "update assert after2");
         },
         builder: (_, _$) => child);
+  }
+
+  void _update(ScrollMetrics scrollMetrics, {bool sync = false}) {
+    if (_onUpdate == null || _updateMetricsFlag) return;
+    final updater = _onUpdate!;
+    _updateMetricsFlag = true;
+
+    void syncUpdate(_) {
+      _updateMetricsFlag = false;
+      _updateScrollData(scrollMetrics);
+      updater(_scrollDataInterface!);
+    }
+
+    if (sync) {
+      syncUpdate('sync');
+    } else {
+      WidgetsBinding.instance!.addPostFrameCallback(syncUpdate);
+    }
+  }
+
+  bool _isNeedUpdateIndex(int index, double size) {
+    final length = _getSizeLength();
+    if (index < length) {
+      final ComputedSize oldComputedSize = _getComputedSize(index);
+      return oldComputedSize.size != size;
+    }
+    return true;
+  }
+
+  //布局时调用
+  void _markUpdateSizesAfterLayout() {
+    if (_updateSizeFlag) return;
+
+    _updateSizeFlag = true;
+
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      _updateSizeFlag = false;
+      final Map cacheSizes = _cacheSizes!;
+
+      int minIndexLayout = _minIndexLayout!;
+      final int maxIndexLayout = _maxIndexLayout!;
+      _minIndexLayout = null;
+      _maxIndexLayout = null;
+
+      assert(minIndexLayout >= 0);
+      assert(minIndexLayout <= maxIndexLayout);
+
+      print("start update Layout => minIndexLayout: $minIndexLayout, maxIndexLayout: $maxIndexLayout");
+
+      do {
+        final size = cacheSizes[minIndexLayout] ?? _getComputedSize(minIndexLayout).size;
+        print("start update => $minIndexLayout => cacheSizes[minIndexLayout]: ${cacheSizes[minIndexLayout]}, size: $size");
+        _updateHeightByLayout(
+          minIndexLayout,
+          size,
+          minIndexLayout == maxIndexLayout,
+        );
+      } while (++minIndexLayout <= maxIndexLayout);
+
+      cacheSizes.clear();
+      _cacheSizes = null;
+
+      //重新寻找
+      _gestureDirection = GestureDirection.forward;
+      //需要将位置清空重新计算
+      _firstVisibleIndex = null;
+      _lastVisibleIndex = null;
+      _nearCenterVisibleIndex = null;
+
+      final ScrollMetrics scrollMetrics =
+          _scrollController?.position ?? _scrollMetrics!;
+      _update(scrollMetrics, sync: true);
+    });
   }
 
   List<ComputedSize> get _visibleComputedSizes {
@@ -195,13 +320,14 @@ mixin _ListLayoutManager on _ListSizeManager {
     return currentIndex;
   }
 
-  _nearCenterIndexComputer(double extentBefore, double viewportDimension) {
+  _nearCenterIndexComputer(double pixels, double viewportDimension) {
     double? bestNearPosition;
     return (int index) {
-      final centerBaselinePosition = extentBefore + viewportDimension / 2;
+      final centerBaselinePosition = pixels + viewportDimension / 2;
       final ComputedSize computedSize = _getComputedSize(index);
       final nearPosition =
           (computedSize.centerPosition - centerBaselinePosition).abs();
+      print("start computer#$index=> centerBaselinePosition:$centerBaselinePosition, nearPosition: $nearPosition, pixels:$pixels, viewportDimension: $viewportDimension");
       if (bestNearPosition == null) {
         bestNearPosition = nearPosition;
         return true;
@@ -242,33 +368,40 @@ mixin _ListLayoutManager on _ListSizeManager {
     final pixels = metrics.pixels;
     final viewportDimension = metrics.viewportDimension;
 
+    _scrollMetrics = metrics;
     _currentPosition ??= 0;
     final double oldPosition = _currentPosition!;
-    _gestureDirection = pixels > oldPosition
-        ? GestureDirection.forward
-        : GestureDirection.backward;
+    _gestureDirection = pixels == oldPosition
+        ? null
+        : (pixels > oldPosition
+            ? GestureDirection.forward
+            : GestureDirection.backward);
     _currentPosition = pixels;
 
-    final _oldFirstVisibleIndex = _firstVisibleIndex ?? 0;
+    _firstVisibleIndex ??= 0;
     if (_nearCenterVisibleIndex == null || _lastVisibleIndex == null) {
       _initCenterAndLastVisibleIndex(
-          _oldFirstVisibleIndex, pixels, viewportDimension);
+          _firstVisibleIndex!, pixels, viewportDimension);
       assert(_nearCenterVisibleIndex != null);
       assert(_lastVisibleIndex != null);
     }
 
-    final nearCenterIndexComputer =
-        _nearCenterIndexComputer(pixels, viewportDimension);
+    if (_gestureDirection != null) {
+      final nearCenterIndexComputer =
+          _nearCenterIndexComputer(pixels, viewportDimension);
 
-    _firstVisibleIndex = _findVisibleIndex(
-        _oldFirstVisibleIndex, pixels, viewportDimension, maxScrollExtent,
-        reverse: false);
-    _lastVisibleIndex = _findVisibleIndex(
-        _lastVisibleIndex!, pixels, viewportDimension, maxScrollExtent,
-        reverse: true);
-    _nearCenterVisibleIndex = _findVisibleIndex(
-        _nearCenterVisibleIndex!, pixels, viewportDimension, maxScrollExtent,
-        otherCondition: nearCenterIndexComputer);
+      _firstVisibleIndex = _findVisibleIndex(
+          _firstVisibleIndex!, pixels, viewportDimension, maxScrollExtent,
+          reverse: false);
+      _lastVisibleIndex = _findVisibleIndex(
+          _lastVisibleIndex!, pixels, viewportDimension, maxScrollExtent,
+          reverse: true);
+      print("!!!!!computerCenterIndex: CenterIndex=>$_nearCenterVisibleIndex");
+      _nearCenterVisibleIndex = _findVisibleIndex(
+          _nearCenterVisibleIndex!, pixels, viewportDimension, maxScrollExtent,
+          otherCondition: nearCenterIndexComputer);
+      print("_nearCenterVisibleIndex: =>=>=>$_nearCenterVisibleIndex");
+    }
   }
 
   void _clearData() {
@@ -277,39 +410,46 @@ mixin _ListLayoutManager on _ListSizeManager {
     _lastVisibleIndex = null;
     _nearCenterVisibleIndex = null;
     _currentPosition = null;
+    _onUpdate = null;
+    _scrollController = null;
+    _minIndexLayout = null;
+    _maxIndexLayout = null;
+    _cacheSizes?.clear();
+    _cacheSizes = null;
   }
 
   Widget buildList(
       {required ScrollView child,
+      OnUpdate? onUpdate,
       OnScrollStart? onScrollStart,
       OnScrollUpdate? onScrollUpdate,
       OnScrollEnd? onScrollEnd}) {
+    _clearData();
     if (_axis != child.scrollDirection) {
       _axis = child.scrollDirection;
       _clearSizes();
     }
 
-    _clearData();
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
-    });
+    if (child.controller != null) {
+      _scrollController = child.controller;
+      WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+        final ScrollPosition position = child.controller!.position;
+        assert(position.hasViewportDimension);
+        _update(position);
+      });
+    }
+    _onUpdate = onUpdate;
     return NotificationListener(
       onNotification: (Notification notification) {
         if (notification is ScrollStartNotification) {
-          final ScrollMetrics metrics = notification.metrics;
-          _firstVisibleIndex ??= 0;
-          if (_lastVisibleIndex == null || _nearCenterVisibleIndex == null) {
-            _initCenterAndLastVisibleIndex(
-                _firstVisibleIndex!, metrics.pixels, metrics.viewportDimension);
-            assert(_lastVisibleIndex != null);
-            assert(_nearCenterVisibleIndex != null);
-          }
-          onScrollStart?.call(_scrollDataInterface, metrics);
+          _updateScrollData(notification.metrics);
+          onScrollStart?.call(_scrollDataInterface!);
         } else if (notification is ScrollUpdateNotification) {
-          final ScrollMetrics metrics = notification.metrics;
-          _updateScrollData(metrics);
-          onScrollUpdate?.call(_scrollDataInterface, metrics);
+          _updateScrollData(notification.metrics);
+          onScrollUpdate?.call(_scrollDataInterface!);
         } else if (notification is ScrollEndNotification) {
-          onScrollEnd?.call(_scrollDataInterface, notification.metrics);
+          _updateScrollData(notification.metrics);
+          onScrollEnd?.call(_scrollDataInterface!);
         }
         return false;
       },
@@ -325,6 +465,10 @@ mixin _ListSizeManager {
     assert(index >= 0);
     assert(index < _getSizeLength());
     _sizes!.removeAt(index);
+    if (index > 0) {
+      final preSize = _sizes![index - 1].size;
+      _updateHeightByLayout(index, preSize, true);
+    }
   }
 
   ComputedSize _getComputedSize(int index) {
@@ -341,7 +485,12 @@ mixin _ListSizeManager {
     _sizes?.clear();
   }
 
-  void _updateHeightByLayout(int index, double size) {
+  void dispose() {
+    _clearSizes();
+    _sizes = null;
+  }
+
+  void _updateHeightByLayout(int index, double size, bool alignPosition) {
     assert(index >= 0);
     _sizes ??= [];
 
@@ -356,7 +505,23 @@ mixin _ListSizeManager {
     final newComputedSize = ComputedSize._(index, size, position);
 
     if (_sizes!.length > index) {
-      _sizes![index] = newComputedSize;
+      final oldComputedSize = _sizes![index];
+      if (oldComputedSize != newComputedSize) {
+        _sizes![index] = newComputedSize;
+        print("update==> index: $index => newComputedSize: $newComputedSize, oldComputedSize: $oldComputedSize, alignPosition: $alignPosition");
+      }
+
+      if (alignPosition) {
+        //对齐位置
+        final length = _getSizeLength();
+        ComputedSize afterComputedSize = newComputedSize;
+        while (++index < length) {
+          afterComputedSize = _sizes![index].copyWith(
+              position: afterComputedSize.position + afterComputedSize.size);
+          print("update==> index: $index => newComputedSize: $afterComputedSize, oldComputedSize: ${_sizes![index]}, alignPosition: $alignPosition");
+          _sizes![index] = afterComputedSize;
+        }
+      }
     } else {
       assert(_sizes!.length == index);
       _sizes!.add(newComputedSize);
@@ -384,6 +549,7 @@ abstract class _ListDataProxy
     _dataList.clear();
     _clearData();
     _clearSizes();
+    super.dispose();
   }
 
   dynamic operator [](int index) => _dataList[index];
@@ -400,6 +566,11 @@ class ComputedSize {
   int get hashCode => hashValues(index, size, position);
 
   double get centerPosition => position + size / 2;
+
+  ComputedSize copyWith({int? index, double? size, double? position}) {
+    return ComputedSize._(
+        index ?? this.index, size ?? this.size, position ?? this.position);
+  }
 
   @override
   bool operator ==(Object other) {
