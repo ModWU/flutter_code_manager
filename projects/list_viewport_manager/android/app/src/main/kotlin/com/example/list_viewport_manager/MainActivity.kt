512105cf927b967c7759714c3765d5d8efcf5431
wuchaochao <wuchaochao@shandagames.com>
2021-05-06 16:41:35 +0800

    delete unserful files

--- projects/list_viewport_manager/android/app/src/main/kotlin/com/example/list_viewport_manager/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.list_viewport_manager
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- projects/wc_cloudbase/android/app/src/main/kotlin/com/example/wc_cloudbase/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.wc_cloudbase
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
